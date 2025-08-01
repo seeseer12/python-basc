b42c76ebf1944849d5c9971ac3759f2010506adb
seeseer12 <bseeseer@gmail.com>
2025-08-01 18:49:10 +0545

    added sum rec

--- basic/recursion.py
+++ basic/recursion.py
@@ -1,11 +1,21 @@
-x=int(input("enter the no"))
-def n(a):
-    print(a)
-    if (a==0):
-     return
+# x=int(input("enter the no"))
+# def n(a):
+#     print(a)
+#     if (a==0):
+#      return
+#     else:
+#        return n(a-1)
+
+
+# n(x)
+
+
+def sum(n):
+    if n == 0:
+        return 0
     else:
-       return n(a-1)
+        return n + sum(n - 1)           
 
-
-n(x)
+n=input("Enter a number: ")
+print("Sum of first", n, "natural numbers is:", sum(int(n)))    
     
